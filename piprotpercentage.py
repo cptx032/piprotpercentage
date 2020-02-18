af8d12feb31205c316f28748e8a6d50e5eb1a6d2
Willie L P Silva <cptx032@gmail.com>
2020-02-18 08:30:12 -0300

    setup script

--- piprotpercentage.py
+++ piprotpercentage.py
@@ -3,10 +3,13 @@
 
 from __future__ import print_function, unicode_literals
 
+import subprocess
 import sys
-import os
 
-OUTDATED_COMMAND = "pip list -o --disable-pip-version-check --no-color"
+OUTDATED_COMMAND = (
+    "pip list -o --disable-pip-version-check --no-color "
+    "--no-python-version-warning"
+).split()
 
 
 def get_requirements_libs(filepath):
@@ -20,19 +23,17 @@ def get_requirements_libs(filepath):
 
 def get_rotten_libs():
     # the first two lines are the headers markers
-    output = os.popen(OUTDATED_COMMAND).read()
-    return [
-        i.split()[0]
-        for i in output.split("\n")[2:]
-        if i
-    ]
+    output, err = subprocess.Popen(
+        OUTDATED_COMMAND, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+    ).communicate()
+    return [i.split()[0] for i in output.split("\n")[2:] if i]
 
 
 def get_rotten_percentage(requirements_path):
     req_libs = set(get_requirements_libs(requirements_path))
     rotten_libs = req_libs.intersection(set(get_rotten_libs()))
-    # import ipdb; ipdb.set_trace(context=10)
     return int((len(rotten_libs) / float(len(req_libs))) * 100)
 
+
 if __name__ == "__main__":
-    print(get_rotten_percentage(sys.argv[1]))
+    print (get_rotten_percentage(sys.argv[1]))
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='piprotpercentage',
+    version='0.1.0',
+    scripts=['piprotpercentage.py'],
+    description='Piprot percentage',
+    author='Willie Lawrence',
+    author_email='cptx032@gmail.com',
+    url='https://github.com/cptx032/piprotpercentage/',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+    ])
